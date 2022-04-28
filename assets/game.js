bc49efa29bca9e9025d990197c8f66f61650cc08
AlexIglesia <iglesia.alexandra@gmail.com>
2022-04-28 11:43:05 +0000

    fix bug to show questions in game

--- assets/game.js
+++ assets/game.js
@@ -12,7 +12,7 @@ let availableQuestions = []
 
 let questions = [
    {
-       question: What country has the highest life expectancy?,
+       question: "What country has the highest life expectancy?",
        choice1: 'Japan',
        choice2: 'Singapore',
        choice3: 'China',
@@ -20,13 +20,13 @@ let questions = [
        answer:3,
     },
    {
-    question: Which language has the more native speakers: English or Spanish?,
+    question: "Which language has the more native speakers: English or Spanish?",
     choice1: 'English',
     choice2: 'Spanish',
     answer:2,
    },
    {
-    question: Who has won the most total Academy Awards?,
+    question: "Who has won the most total Academy Awards?",
     choice1: 'Walt Disney',
     choice2: 'Katharine Hepburn',
     choice3: 'Meryl Streep',
@@ -34,7 +34,7 @@ let questions = [
     answer:1,
    },
    {
-    question: What country drinks the most coffee per capita?,
+    question: "What country drinks the most coffee per capita?",
     choice1: 'Finland',
     choice2: 'Colombia',
     choice3: 'United States',
@@ -42,7 +42,7 @@ let questions = [
     answer:1,
    },
    {
-    question: What is a group of pandas known as?,
+    question: "What is a group of pandas known as?",
     choice1: 'a band',
     choice2: 'a business',
     choice3: 'an embarrassment',
@@ -50,7 +50,7 @@ let questions = [
     answer:3,
    },
    {
-    question: How many bones do we have in an ear?,
+    question: "How many bones do we have in an ear?",
     choice1: '0',
     choice2: '1',
     choice3: '2',
@@ -58,7 +58,7 @@ let questions = [
     answer:3,
    },
 {
-    question: What Netflix show had the most streaming views in 2021?,
+    question: "What Netflix show had the most streaming views in 2021?",
        choice1: 'Squid Game',
        choice2: 'Lupin part one',
        choice3: 'Money Heist season five',
@@ -66,22 +66,23 @@ let questions = [
        answer:1,
 },
 {
-    question: What is the world's fastest bird?,
+    question: "What is the world\'s fastest bird?",
        choice1: 'Golden eagle',
-       choice2: 'Anna's hummingbird',
+       choice2: 'Anna\'s hummingbird',
        choice3: 'Grey-headed albatross',
        choice4: 'The Peregrine Falcon',
        answer:4,
 },
 {
-    question: In what country is the Chernobyl nuclear plant located?,
-       choice1: 'Japan',
+    question: "In what country is the Chernobyl nuclear plant located?",
+       choice1: 'Ukraine',
        choice2: 'Singapore',
        choice3: 'China',
-       choice4: 'Switzerland'
+       choice4: 'Switzerland',
+       answer:1,
 },
 {
-    question: In what country would you find Mount Kilimanjaro?,
+    question: "In what country would you find Mount Kilimanjaro?",
        choice1: 'Argentina',
        choice2: 'Mexico',
        choice3: 'Tanzania',
@@ -89,11 +90,78 @@ let questions = [
        answer:3,
 },
 {
-    question: What company was initially know as Blue Ribbon Sports?,
+    question: "What company was initially know as Blue Ribbon Sports?",
        choice1: 'Adidas',
        choice2: 'Nike',
        choice3: 'Reebook',
        choice4: 'New Balance',
        answer:2,
-}
+   }
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 10
+
+startGame = () => {
+   questionCounter = 0
+   score = 0
+   availableQuestions = [...questions]
+   getNewQuestion()
+}
+
+getNewQuestion = () => {
+   if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+      localStorage.setItem('mostRecentScore', score);
+
+      return window.location.assign('/end.html')
+   }
+   
+   questionCounter++
+   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+   progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+   const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+   currentQuestion = availableQuestions[questionsIndex]
+   question.innerText = currentQuestion.question
+
+
+   choices.forEach(choice => {
+      const number = choice.dataset['number']
+      choice.innerText = currentQuestion['choice' + number]
+   })
+
+   availableQuestions.splice(questionsIndex, 1)
+
+   acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+   choice.addEventListener('click', e => {
+      if(!acceptingAnswers) return
+
+      acceptingAnswers = false
+      const selectedChoice = e.target
+      const selectedAnswer = selectedChoice.dataset['number']
+   
+      let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+      if(classToApply === 'correct') {
+         incrementScore(SCORE_POINTS)
+      }
+
+
+      selectedChoice.parentElement.classList.add(classToApply)
+
+      setTimeout(() => {
+         selectedChoice.parentElement.classList.remove(classToApply)
+         getNewQuestion()
+      }, 1000)
+   })
+})
+
+incrementScore = num => {
+   score +=num
+   scoreText.innerText = score
+}
+
+startGame()
